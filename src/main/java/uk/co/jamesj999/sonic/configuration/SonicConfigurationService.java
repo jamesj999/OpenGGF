54c8e2cfe7a37c8fda7087a54b3535736cdcffcc
James <james.johnstone@bravebison.com>
2026-02-11 15:07:16 +0000

    Bumping version number to 0.4.prerelease

--- src/main/java/uk/co/jamesj999/sonic/configuration/SonicConfigurationService.java
+++ src/main/java/uk/co/jamesj999/sonic/configuration/SonicConfigurationService.java
@@ -14,7 +14,7 @@ import static org.lwjgl.glfw.GLFW.*;
 
 public class SonicConfigurationService {
 	private static SonicConfigurationService sonicConfigurationService;
-	public static String ENGINE_VERSION = "0.3.20260206";
+	public static String ENGINE_VERSION = "0.4.prerelease";
 
 	private Map<String, Object> config;
 
