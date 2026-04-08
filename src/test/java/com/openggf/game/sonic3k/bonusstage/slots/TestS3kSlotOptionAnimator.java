3cd0671d9daed0bc5a5a24b263d8d4b216c0c3a8
Farrell <farrell@bluetoaster.net>
2026-04-08 03:07:15 +0100

    Add S3K slot machine regression tests

--- src/test/java/com/openggf/game/sonic3k/bonusstage/slots/TestS3kSlotOptionAnimator.java
+++ src/test/java/com/openggf/game/sonic3k/bonusstage/slots/TestS3kSlotOptionAnimator.java
@@ -12,4 +12,24 @@ class TestS3kSlotOptionAnimator {
         assertEquals(0x0240, S3kSlotOptionAnimator.sourceOffsetFor(1, 0x0080));
         assertEquals(0x0C7C, S3kSlotOptionAnimator.sourceOffsetFor(6, 0x00F8));
     }
+
+    @Test
+    void symbolValuesWrapWithinTheEightEntryTable() {
+        assertEquals(
+                S3kSlotOptionAnimator.sourceOffsetFor(0, 0x0000),
+                S3kSlotOptionAnimator.sourceOffsetFor(8, 0x0000));
+        assertEquals(
+                S3kSlotOptionAnimator.sourceOffsetFor(7, 0x00F8),
+                S3kSlotOptionAnimator.sourceOffsetFor(15, 0x00F8));
+    }
+
+    @Test
+    void lowReelBitsAreIgnoredByTheMask() {
+        assertEquals(
+                S3kSlotOptionAnimator.sourceOffsetFor(3, 0x00F8),
+                S3kSlotOptionAnimator.sourceOffsetFor(3, 0x00FF));
+        assertEquals(
+                S3kSlotOptionAnimator.sourceOffsetFor(5, 0x0080),
+                S3kSlotOptionAnimator.sourceOffsetFor(5, 0x0087));
+    }
 }
